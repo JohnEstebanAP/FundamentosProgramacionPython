c9315fd0fb565ccd10638a6ffa453a85a41bcd61
John Esteban Alvarez Piedrahita <esteban.ea145@gmail.com>
2021-06-03 18:25:22 -0500

    sesión 10.1

---
+++ Sesión10/prueba.py
@@ -0,0 +1,14 @@
+
+def sumar (n1,n2):
+    
+    suma = n1 + n2
+
+    print(f"{n1} + {2} = {suma}.")
+
+def restar(n1,n2):
+    resta = n1 - n2
+    return resta
+
+
+sumar(5,5)
+print(restar(5-2))
